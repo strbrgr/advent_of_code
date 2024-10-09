21a9ebb3cd2d7695333d8a1780dab91953f0f72f
Jochen Stierberger <jocsti@web.de>
2024-10-09 07:41:52 -0500

    feat: Finish Part 1 and improve Error handling along the way

--- 2018/d4/src/main.rs
+++ 2018/d4/src/main.rs
@@ -1,6 +1,10 @@
-use std::{collections::HashMap, error::Error, fs};
+use std::error::Error;
+use std::result;
+use std::{collections::HashMap, fs};
 
-fn main() -> Result<(), Box<dyn Error>> {
+type Result<T> = result::Result<T, Box<dyn Error>>;
+
+fn main() -> Result<()> {
     let input = fs::read_to_string("input/final.txt")?;
 
     let mut lines: Vec<&str> = input.lines().collect();
@@ -8,20 +12,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     lines.sort_by_key(|line| extract_date(line).expect("Invalid format"));
 
     match part_1(&lines) {
-        Ok(result) => println!("{result}"),
-        Err(err) => println!("{err}"),
+        Ok(result) => println!("Part 1 result: {}", result),
+        Err(err) => println!("There was an error in Part 1: {}", err),
     };
 
     Ok(())
 }
 
-fn part_1(lines: &Vec<&str>) -> Result<i32, String> {
-    let mut result: HashMap<i32, [i32; 60]> = HashMap::new();
+fn part_1(lines: &Vec<&str>) -> Result<usize> {
+    let mut result: HashMap<usize, [usize; 60]> = HashMap::new();
     let mut sleep_start = 0usize;
-    let mut id = 0;
+    let mut id = 0usize;
 
     for line in lines {
-        let (minute, action) = parse_line(&line)?;
+        let (minute, action) = parse_line(line)?;
 
         match action {
             _ if action.starts_with("Guard ") => {
@@ -29,8 +33,7 @@ fn part_1(lines: &Vec<&str>) -> Result<i32, String> {
                     .split_once(" #")
                     .ok_or("Could not split Guard info")?;
                 let (guard_id, _) = id_content.split_once(' ').ok_or("Could not split id")?;
-                // TODO: Avoid expect
-                id = guard_id.parse::<i32>().expect("Could not parse");
+                id = guard_id.parse::<usize>()?;
             }
 
             _ if action.starts_with("falls asleep") => {
@@ -59,11 +62,11 @@ fn part_1(lines: &Vec<&str>) -> Result<i32, String> {
         }
     }
 
-    let mut biggest_sleeper = 0;
-    let mut sleep_sum = 0;
+    let mut biggest_sleeper = 0usize;
+    let mut sleep_sum = 0usize;
 
     for (k, v) in result.iter() {
-        let sum: i32 = v.iter().sum();
+        let sum: usize = v.iter().sum();
         if sum > sleep_sum {
             biggest_sleeper = *k;
             sleep_sum = sum;
@@ -72,31 +75,30 @@ fn part_1(lines: &Vec<&str>) -> Result<i32, String> {
 
     let sleeper = match result.get(&biggest_sleeper) {
         Some(s) => s,
-        None => return Err(String::from("Could not get value out of HashMap")),
+        None => return Err(From::from("could not find two correct box ids")),
     };
 
-    let max_minute = match sleeper
+    let max_minute = sleeper
         .iter()
         .enumerate()
         .max_by_key(|(_, &value)| value)
         .map(|(idx, _)| idx)
-    {
-        Some(max) => max,
-        None => return Err(String::from("Could get max_minute")),
-    };
+        .ok_or("Could not find the maximum entry")?;
 
-    Ok(biggest_sleeper * max_minute as i32)
+    Ok(biggest_sleeper * max_minute)
 }
 
-fn extract_date(line: &str) -> Result<&str, &'static str> {
-    line.split_once('[')
+fn extract_date(line: &str) -> Result<&str> {
+    let date = line
+        .split_once('[')
         .and_then(|(_, rest)| rest.split_once(']'))
         .map(|(date, _)| date.trim())
-        .ok_or("Invalid format: date not found in brackets")
+        .ok_or("Invalid format: date not found in brackets")?;
+
+    Ok(date)
 }
 
-// TODO: Better Error Return
-fn parse_line(line: &str) -> Result<(usize, &str), String> {
+fn parse_line(line: &str) -> Result<(usize, &str)> {
     let minute_hour = line
         .split_whitespace()
         .nth(1)

6a30dd6e7709276541e8daf272063b8ba6e2b7ad
Jochen Stierberger <jocsti@web.de>
2024-11-16 16:35:43 -0600

    feat: Finish Part 1 of Day 6

--- 2018/d6/src/main.rs
+++ 2018/d6/src/main.rs
@@ -1,49 +1,113 @@
-use std::{error::Error, fs, result};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+    error::Error,
+    fs, result,
+};
 
 type Result<T> = result::Result<T, Box<dyn Error>>;
+type Grid<T> = Vec<Vec<T>>;
 
-// Finite vs infinite?
-// Use Manhattan distance to determine each location's closest coordinate
-// Does it work with a grid that is bound to the coordinates size?
-//
-// I have a certain amount of coordinates (input).
-// Each point on the grid, keeps track of its own position
-// and has a manhattan distance to other coordinates, and keeps track of the smallest
-// If there is more than two matching, we place a dot
-// Use 2d array
-//
-//  Setup function:
-//  find grid size by getting biggest x and y
-//  create 2d array with that
-//  TODO:
-//  loop over each entry in grid, calculate manhattan distance from each point to each other
-//  coordinate, keep track of smallest coordinates char
-//  write enum value to grid, enum value is either character or dot
-//
 // Part 1 should start with the filled out grid and then loop over and do a count
 fn main() -> Result<()> {
-    let input = fs::read_to_string("input/test.txt")?;
-    let (mut max_x, mut max_y) = (usize::MIN, usize::MIN);
-    let mut coordinates: Vec<Vec<usize>> = Vec::new();
+    let input = fs::read_to_string("input/final.txt")?;
+    let (mut max_x, mut max_y) = (i32::MIN, i32::MIN);
+    // Holds vectors of coordinates
+    let mut coordinates: Vec<Vec<i32>> = Vec::new();
 
-    // Get Grid values
+    // Creat Grid
+    // TODO: Hanlde parsing error better
     for line in input.lines() {
         let coordinate = line
             .split(", ")
             .map(|s| s.trim().parse())
-            .collect::<std::result::Result<Vec<usize>, _>>()?;
+            .collect::<std::result::Result<Vec<i32>, _>>()?;
 
+        // Find the maximum x/y value so that we can create the grid
         if coordinate.len() == 2 {
-            let (x, y) = (coordinate[0], coordinate[1]);
-            max_x = max_x.max(x);
-            max_y = max_y.max(y);
+            // I'm okay with unwrap after checking for length
+            let (x, y) =
+                (coordinate.first().unwrap(), coordinate.get(1).unwrap());
+            max_x = max_x.max(*x);
+            max_y = max_y.max(*y);
         }
+
+        coordinates.push(coordinate);
     }
 
-    // Create grid with default values
-    let grid: Vec<Vec<String>> = vec![vec![String::new(); max_x]; max_y];
-    println!("{:?}", grid);
+    // Adds a buffer
+    max_x += 1;
+    max_y += 1;
+    let mut grid: Grid<String> =
+        vec![vec![String::new(); max_x as usize]; max_y as usize];
+
+    match part_1(&mut grid, &coordinates) {
+        Ok(result) => println!("Part 1 result: {}", result),
+        Err(err) => return Err(err),
+    }
     Ok(())
 }
 
-// fn manhattan_distance
+fn part_1(grid: &mut Grid<String>, coordinates: &Grid<i32>) -> Result<i32> {
+    let mut infinite_values = HashSet::new();
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            let mut minimum_distance = i32::MAX;
+            let mut minimum_index = None;
+            let mut conflict = false;
+
+            for (i, el) in coordinates.iter().enumerate() {
+                let manhattan_distance =
+                    (row as i32 - el[1]).abs() + (col as i32 - el[0]).abs();
+
+                match manhattan_distance.cmp(&minimum_distance) {
+                    Ordering::Less => {
+                        minimum_distance = manhattan_distance;
+                        minimum_index = Some(i);
+                        conflict = false;
+                    }
+                    Ordering::Equal => {
+                        conflict = true;
+                    }
+                    Ordering::Greater => {
+                        // Do nothing
+                    }
+                }
+            }
+
+            if conflict {
+                grid[row][col] = String::from(".");
+            } else if let Some(index) = minimum_index {
+                let coordinate = (b'A' + index as u8) as char;
+                grid[row][col] = coordinate.to_string();
+            }
+
+            // Check which of the values are infinte
+            if row == 0
+                || row == grid.len() - 1
+                || col == 0
+                || col == grid[0].len() - 1
+            {
+                infinite_values.insert(grid[row][col].to_string());
+            }
+        }
+    }
+
+    let mut finite_values: HashMap<String, i32> = HashMap::new();
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if !infinite_values.contains(&grid[row][col]) {
+                *finite_values
+                    .entry(grid[row][col].to_string())
+                    .or_insert(0) += 1;
+            }
+        }
+    }
+
+    let largest_value = finite_values
+        .iter()
+        .max_by_key(|&(_, v)| v)
+        .ok_or("Could not find an entry")?;
+
+    Ok(*largest_value.1)
+}
